19fb6fca0d8ac3265cee8ff29644707d581486ad
kevinasian122 <yanchenkai@gmail.com>
2022-11-24 22:30:41 -0500

    Used axios to get JSON data from backend server
    imported axios to send requests the express.js backend server for data about restaurants.
    Functions for getAll, get from id, delete id were implemented
    No database is connected yet. Next steps are create production build, deploy and connect db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './index.css';
 import { useState, useEffect } from 'react'
 import Sidebar from './Components/Sidebar'
-import Header from './Components/Header'
+// import Header from './Components/Header'
+import restaurantService from './services/restaurants'
 
+//TODO
+//add styling to each restaurant block to show info better //partially done
+//setup axios request data //done
+//setup backend //done
+//make production build and deploy
+//connect backend with database
+//add delete button
+//seperate into components
+//add scrolling feature to only the restaurants block
+//each restaurant can be clicked to render a component that shows info + google maps API for ratings and travel time
+//form to add restaurant name section uses google maps API to search for the restaurant and stores it
+//implement user login
 function App() {
 
   const [showForm, setShowForm] = useState(false)
@@ -11,7 +24,18 @@ function App() {
   const [newVisited, setNewVisited] = useState('')
   const [newComments, setNewComments] = useState('')
   const [restaurants, setRestaurants] = useState([])
+  const [filter, setNewFilter] = useState ('')
 
+
+  const hook = () => {
+    restaurantService.getAll()
+    .then(initial => {
+      setRestaurants(initial)
+    })
+  }
+  useEffect(hook, [])
+
+  //handle form
   const openForm = () => {
     setShowForm(true)
   }
@@ -30,11 +54,20 @@ function App() {
   const handleCommentsChange = (e) => {
     setNewComments(e.target.value)
   }
+  const handleFilterChange = (e) => {
+    setNewFilter(e.target.value)
+    
+  }
+  const deleteRes = (id) => {
+    const target = restaurants.find(res => id === res.id)
+    if(window.confirm(`Delete ${target}?`)){
+      restaurantService.del(id)
+      setRestaurants(restaurants.filter(r => r.id !== id))
+    }
+  }
   const active = showForm? 'active':'' //for the popup, for some reason having it in seperate component dont focus
   const classes = `modal ${active}`
 
-
-  
    //adding new restaurant
    const addRes = (e) => {
     e.preventDefault()
@@ -45,14 +78,19 @@ function App() {
       comments: newComments,
       id: Math.random()*100
     }
-    setRestaurants(restaurants.concat(newRes))
-    setNewName('')
-    setNewRating('')
-    setNewVisited('')
-    setNewComments('')
-    closeForm()
+    restaurantService.create(newRes)
+    .then(data => {
+      setRestaurants(restaurants.concat(data))
+      setNewName('')
+      setNewRating('')
+      setNewVisited('')
+      setNewComments('')
+      closeForm()
+    })
+    
   }
 
+  
   const Overlay = () => {
     const active = showForm? 'active':''
     const classes = `overlay ${active}`
@@ -61,34 +99,45 @@ function App() {
     )
   }
 
- 
+
+
+  const ShowRes = () => {
+    return resToShow.map(res => 
+      <div key={res.id} className = "restaurant"> <div className = "name"> {res.name}</div> 
+      <div className = "rating"> Rating: {res.rating}/10 </div> 
+      <div className = "timesvisited">Visited: {res.timesVisited}  </div> 
+      <div className = "comments">{res.comments}</div> 
+      <button className="deleteRes" onClick = {() => deleteRes(res.id)}>Delete</button></div>
+    )
+  }
+
+  const resToShow = restaurants.filter(res => res.name.toLowerCase().includes(filter.toLowerCase()))
   return (
     <div className='body'>
       <Sidebar />
       <div className = 'right'>
         <div className="header">
-              <div className="search">
-                  <input type="text" placeholder="Search.." />
-              </div>
+              
+              <input 
+              className="search" type="text" placeholder="Search.." 
+               value = {filter} 
+              onChange={handleFilterChange} 
+              />
+              
               <div className="profile">
-                  <p>Kevin Yan</p>
+                  <button className="account">Kevin Yan</button>
               </div>
               <div className="greeting">
                   <div> Hi there, </div>
                   <div>Kevin</div> 
               </div>
               <div className="buttons">
-                  <button className="upload" onClick={() => openForm()}>+</button>
+                  <button className="upload" onClick={() => openForm()}>Upload</button>
               </div>
           </div>
           <div className="main">
             <div className="content">
-              {restaurants.map(res => 
-                <div key={res.id} className = "restaurant"> <div className = "name"> {res.name}</div> 
-                <div className = "rating"> My Rating: {res.rating}/10 </div> 
-                <div className = "timesvisited">Times Visited: {res.timesvisited}  </div> 
-                <div className = "comments">My Thoughts: {res.comments}</div> </div>
-              )}
+              <ShowRes />
             </div>
           </div>
       </div>
@@ -97,7 +146,7 @@ function App() {
           <form action = "" className = "add-restaurant" onSubmit = {addRes}>
               <h2>New Restaurant</h2>
               <input
-              className = "input"
+              className = "getName"
               type = "text"
               value={newName}
               onChange={handleNameChange}
@@ -106,17 +155,17 @@ function App() {
               maxLength = "200" />
 
               <input
-              className = "rating"
+              className = "getRating"
               type = "number"
               value = {newRating}
               onChange={handleRatingChange}
-              placeholder = "Rating"
+              placeholder = "Rating / 10"
               required
               min = "0"
               max = "10" />
 
               <input
-              className = "timesvisited"
+              className = "getTimesvisited"
               type = "number"
               value = {newVisited}
               onChange={handleVisitedChange}
@@ -125,7 +174,7 @@ function App() {
               min = "0" />
 
               <input
-              className = "comments"
+              className = "getComments"
               type = "text"
               value = {newComments}
               onChange={handleCommentsChange}
